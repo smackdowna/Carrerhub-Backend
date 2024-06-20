5bf34b356e38d11497cb6d36df35a063adad2e69
Sandip Sapkota <079bct076@ioepc.edu.np>
2024-06-20 17:55:18 +0545

    chore: Update employeerController to use ImageKit for resume upload and deletion

---
+++ config/image-kit.js
@@ -0,0 +1,9 @@
+var ImageKit = require("imagekit");
+
+const ik = new ImageKit({
+  urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
+  publicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGE_KIT_PRIVATE_KEY,
+});
+
+module.exports = ik;
--- controllers/employeerController.js
+++ controllers/employeerController.js
@@ -7,6 +7,7 @@ const cloudinary = require("cloudinary");
 const sendEmail = require("../utils/sendEmail.js");
 const getDataUri = require("../utils/dataUri.js");
 const { EMPLOYER_AUTH_TOKEN } = require("../constants/cookies.constant");
+const ik = require("../config/image-kit.js");
 const fs = require("fs");
 
 async function deleteUsersWithExpiredOTP() {
@@ -321,29 +322,46 @@ exports.updateEmployeerDetails = catchAsyncErrors(async (req, res, next) => {
 
   if (file) {
     const fileUri = getDataUri(file);
-    const mycloud = await cloudinary.v2.uploader.upload(fileUri.content, {
-      folder: "company_avatar",
-      width: 150,
-      crop: "scale",
-    });
+    // const mycloud = await cloudinary.v2.uploader.upload(fileUri.content, {
+    //   folder: "company_avatar",
+    //   width: 150,
+    //   crop: "scale",
+    // });
 
-    // Destroy existing avatar if present
-    if (user.company_avatar.public_id) {
-      await cloudinary.v2.uploader.destroy(user.company_avatar.public_id);
-    }
-
-    user.company_avatar = {
-      public_id: mycloud.public_id,
-      url: mycloud.secure_url,
-    };
+    ik.upload(
+      {
+        file: fileUri.content,
+        fileName: fileUri.fileName,
+        folder: "Employeer_Details",
+        useUniqueFileName: true,
+      },
+      async function (error, result) {
+        console.log(result);
+        if (error) {
+          res.status(400).json({
+            success: false,
+            message: "Profile update failed! Please try again later.",
+          });
+        } else {
+          if (user.company_avatar.public_id && user.company_avatar.url) {
+            await ik.deleteFile(user.company_avatar.public_id);
+            await ik.deleteFile(user.company_avatar.thumbnailUrl);
+            //await cloudinary.v2.uploader.destroy(user.company_avatar.public_id);
+          }
+          user.company_avatar = {
+            public_id: result.fileId,
+            url: result.url,
+            thumbnailUrl: result.thumbnailUrl,
+          };
+          await user.save();
+          res.status(200).json({
+            success: true,
+            message: "Profile is updated successfully ",
+          });
+        }
+      }
+    );
   }
-
-  await user.save();
-
-  res.status(200).json({
-    success: true,
-    message: "Profile is updated successfully ",
-  });
 });
 
 // update Employeer password
