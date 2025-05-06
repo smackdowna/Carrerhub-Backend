a74728b10e4c51e4c813d2c6de367e9e4fa4fb4e
Rahul Sutradhar <138353163+rahulsd380@users.noreply.github.com>
2025-05-06 02:22:55 -0700

    Update employeer.js

--- models/employeer.js
+++ models/employeer.js
@@ -29,11 +29,11 @@ const employeerSchema = new mongoose.Schema({
   },
   address: [
     {
-      street: { type: String, required: true },
-      city: { type: String, required: true },
-      state: { type: String, required: true },
-      postalCode: { type: String, required: true },
-      country: { type: String, required: true },
+      street: { type: String },
+      city: { type: String},
+      state: { type: String },
+      postalCode: { type: String},
+      country: { type: String },
     },
   ],
   companyDetails: [
